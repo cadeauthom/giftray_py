c26e659223901c28d037eaccd1003a4ba1d4f2f8
cadeauthom <cadeauthom@gmail.com>
2022-02-02 02:05:38 +0100

    starting menu

--- src/giftray.py
+++ src/giftray.py
@@ -59,22 +59,21 @@ def GetIcon(path,ico="default_default.ico"):
     return win32gui.LoadIcon(0, win32con.IDI_APPLICATION)
 
 
-#keyboard.add_hotkey('ctrl + shift + z', print, args =('Hotkey', 'Detected')) 
+#keyboard.add_hotkey('ctrl + shift + z', print, args =('Hotkey', 'Detected'))
 class MainClass(object):
     def __init__(self):
         self._begin()
         self._reset()
         self._readconf()
         self._useconf()
-        self._PopUp("f","a")
 
     def _begin(self):
         self.showname           = "GifTray"
         self.name               = "giftray"
-        message_map = {
-                win32con.WM_DESTROY: self.OnDestroy,
-                # win32con.WM_CLOSE: self.onClose
-        }
+        message_map = {win32gui.RegisterWindowMessage("TaskbarCreated"): self._restart,
+                       win32con.WM_DESTROY: self._destroy,
+                       win32con.WM_COMMAND: self._command,
+                       win32con.WM_USER+20 : self._notify,}
         # Register the Window class.
         wc = win32gui.WNDCLASS()
         hinst = wc.hInstance = win32gui.GetModuleHandle(None)
@@ -138,24 +137,75 @@ class MainClass(object):
         nid = (self.hwnd, 0, flags, win32con.WM_USER+20, self.main_hicon, self.name)
         win32gui.Shell_NotifyIcon(win32gui.NIM_MODIFY, nid)
 
-    def _PopUp(self, title, msg):
+    def show_menu(self):
+        menu = win32gui.CreatePopupMenu()
+        self.create_menu(menu, self.menu_options)
+        #win32gui.SetMenuDefaultItem(menu, 1000, 0)
+
+        pos = win32gui.GetCursorPos()
+        # See http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winui/menus_0hdi.asp
+        win32gui.SetForegroundWindow(self.hwnd)
+        win32gui.TrackPopupMenu(menu,
+                                win32con.TPM_LEFTALIGN,
+                                pos[0],
+                                pos[1],
+                                0,
+                                self.hwnd,
+                                None)
+        win32gui.PostMessage(self.hwnd, win32con.WM_NULL, 0, 0)
+
+    def create_menu(self, menu, menu_options):
+        for option_text, option_icon, option_action, option_id in menu_options[::-1]:
+            if option_icon:
+                option_icon = self.prep_menu_icon(option_icon)
+
+            if option_id in self.menu_actions_by_id:
+                item, extras = win32gui_struct.PackMENUITEMINFO(text=option_text,
+                                                                hbmpItem=option_icon,
+                                                                wID=option_id)
+                win32gui.InsertMenuItem(menu, 0, 1, item)
+            else:
+                submenu = win32gui.CreatePopupMenu()
+                self.create_menu(submenu, option_action)
+                item, extras = win32gui_struct.PackMENUITEMINFO(text=option_text,
+                                                                hbmpItem=option_icon,
+                                                                hSubMenu=submenu)
+                win32gui.InsertMenuItem(menu, 0, 1, item)
+
+    def _popup(self, title, msg):
         win32gui.Shell_NotifyIcon(win32gui.NIM_MODIFY, \
                          (self.hwnd, 0, win32gui.NIF_INFO,  win32con.WM_USER+20,\
                           self.main_hicon, "",msg,400,title, win32gui.NIIF_NOSOUND))
 
-    def onClose(self, hwnd): #to be called from thread as a method of the class instance
-        win32gui.DestroyWindow(hwnd)
-        classAtom = win32gui.UnregisterClass(classAtom, hinst)
+    def _notify(self, hwnd, msg, wparam, lparam):
+        if lparam==win32con.WM_LBUTTONDBLCLK:
+            pass
+            #self.execute_menu_option(self.default_menu_index + self.FIRST_ID)
+        elif lparam==win32con.WM_RBUTTONUP:
+            self._show_menu()
+        elif lparam==win32con.WM_LBUTTONUP:
+            pass
         return True
 
-    def OnDestroy(self, hwnd, msg, wparam, lparam):
+    def _destroy(self, hwnd, msg, wparam, lparam):
         nid = (self.hwnd, 0)
-        win32gui.Shell_NotifyIcon(NIM_DELETE, nid)
-        PostQuitMessage(0) # Terminate the app.
+        win32gui.Shell_NotifyIcon(win32gui.NIM_DELETE, nid)
+        win32gui.PostQuitMessage(0) # Terminate the app.
+
+    def _command(self, hwnd, msg, wparam, lparam):
+        id = win32gui.LOWORD(wparam)
+        self.execute_menu_option(id)
+
+    def _restart(self, hwnd, msg, wparam, lparam):
+        return True
+
+    def wait(self):
+        win32gui.PumpMessages()
 
 if __name__ == '__main__':
     #import itertools, glob
-    MainClass()
+    a=MainClass()
+    a.wait()
 
     #ball=WindowsBalloonTip.BalloonTip("my test","this is my test", ico="../dist/giftray/icons/black/giftray-0.ico",taskbarname="GifTray-Tip",time=20)
     #ball.popup()
