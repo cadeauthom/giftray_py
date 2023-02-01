2066d4eca52cab8572aab6e381774e3f22562e8a
cadeauthom <cadeauthom@gmail.com>
2023-02-01 01:48:29 +0100

    msi OK but without submodules

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 # base="Win32GUI" should be used only for Windows GUI app
 base = "Win32GUI" if sys.platform == "win32" else None
 setup(executables=[Executable(
-                    "giftray.py",
+                    "main.py",
 #"src/icon.py",
                     base=base,
                     icon="../build/icons/giftray.ico"
